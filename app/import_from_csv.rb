937a5e6180e213c12e42d08b42bea3e81cab0222
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-12-22 18:17:45 -0300

    cria um modulo para ImportFromCSV

--- app/import_from_csv.rb
+++ app/import_from_csv.rb
@@ -2,51 +2,59 @@ require 'csv'
 require 'sinatra/activerecord'
 require './app/models/exam'
 
-rows = CSV.read("./data.csv", col_sep: ';')
-
-columns = rows.shift
-
-def get_db_column_name(name)
-  case name
-  when 'cpf'
-    :cpf
-  when 'nome paciente'  
-    :patient_name
-  when 'email paciente'  
-    :patient_email
-  when 'data nascimento paciente'  
-    :patient_bithdate
-  when 'endereço/rua paciente'  
-    :patient_address
-  when 'cidade paciente'  
-    :patient_city
-  when 'estado patiente'  
-    :patient_state
-  when 'crm médico'  
-    :doctor_crm
-  when 'crm médico estado'  
-    :doctor_crm_state
-  when 'nome médico'  
-    :doctor_name
-  when 'email médico'  
-    :doctor_email
-  when 'token resultado exame'  
-    :result_token
-  when 'data exame'  
-    :exam_date
-  when 'tipo exame'  
-    :exam_type
-  when 'limites tipo exame'  
-    :exam_type_limit
-  when 'resultado tipo exame'  
-    :exam_type_result
+module ImportFromCsv
+  def get_db_column_name(name)
+    case name
+    when 'cpf'
+      :cpf
+    when 'nome paciente'  
+      :patient_name
+    when 'email paciente'  
+      :patient_email
+    when 'data nascimento paciente'  
+      :patient_bithdate
+    when 'endereço/rua paciente'  
+      :patient_address
+    when 'cidade paciente'  
+      :patient_city
+    when 'estado patiente'  
+      :patient_state
+    when 'crm médico'  
+      :doctor_crm
+    when 'crm médico estado'  
+      :doctor_crm_state
+    when 'nome médico'  
+      :doctor_name
+    when 'email médico'  
+      :doctor_email
+    when 'token resultado exame'  
+      :result_token
+    when 'data exame'  
+      :exam_date
+    when 'tipo exame'  
+      :exam_type
+    when 'limites tipo exame'  
+      :exam_type_limit
+    when 'resultado tipo exame'  
+      :exam_type_result
+    end
   end
-end
 
-rows.map do |row|
-  exam = row.each_with_object({}).with_index do |(cell, acc), idx|
-    column = columns[idx]
-    acc[get_db_column_name(column)] = cell
+  def import(test: false)
+    rows = CSV.read("./data.csv", col_sep: ';')
+
+    columns = rows.shift
+
+    if test
+      rows = rows.first(10)
+    end
+
+    rows.map do |row|
+      exam = row.each_with_object({}).with_index do |(cell, acc), idx|
+        column = columns[idx]
+        acc[get_db_column_name(column)] = cell
+      end
+      Exam.create(exam) unless Exam.where(exam).any?
+    end
   end
-  Exam.create(exam) unless Exam.where(exam).any?
 end
