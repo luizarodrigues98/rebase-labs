205b6781dd740ba61e9451c2ec33355bcc06b944
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-12-27 15:03:10 -0300

    adiciona o sinatra/cors

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/cors'
 Dir["./app/models/*.rb"].each { |file| require file }
+
+set :allow_origin, "*"
 
 get '/tests' do
   Exam.all.to_json
