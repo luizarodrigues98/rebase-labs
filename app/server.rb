2df39251f8c1114ed470b863612cc08f3a9d1515
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2023-01-03 15:29:28 -0300

    modifica rotas e adiciona a lógica para busca

--- app/server.rb
+++ app/server.rb
@@ -4,11 +4,22 @@ require 'sinatra/cors'
 Dir["./app/models/*.rb"].each { |file| require file }
 
 set :allow_origin, "*"
+set :allow_methods, "GET"
+set :allow_headers, "content-type"
 
 get '/tests' do
-  Exam.all.to_json
+  if params.has_key?('query')
+    Exam.where("result_token ILIKE '#{params[:query]}%'").unique_by_token.to_json
+  else
+    Exam.unique_by_token.to_json
+  end
 end
 
-get '/hello' do
-  'Hello world!'
+get '/tests/:token' do
+  if exam = Exam.find_by(result_token: params[:token])
+    exam.attributes.merge(exams: Exam.where(result_token: exam.result_token).to_a).to_json
+  else
+    status 404
+  end
 end
+
