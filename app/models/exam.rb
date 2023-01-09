dbb1873cf5ee517f1b79d265ad3be0afea069b3a
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2023-01-09 15:25:33 -0300

    adiciona validates em exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,10 +1,10 @@
 class Exam < ActiveRecord::Base
   scope :unique_by_token, -> { all.uniq{ |e| e[:result_token] } }
   # Ex:- scope :active, -> {where(:active => true)}
-  validates :patient_name, :patient_email, :patient_address,
+  validates :cpf, :patient_name, :patient_email, :patient_address,
             :patient_city, :patient_state, :patient_bithdate,
             :doctor_crm, :doctor_crm_state, :doctor_email, 
-            :doctor_name, :result_token, :exam_type,
+            :doctor_name, :result_token, :exam_type, :exam_type_result,
              presence: true
-
+  validates :cpf, length: { minimum: 11, maximum: 14 }
 end
